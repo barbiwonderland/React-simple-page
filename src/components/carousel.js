45d1186742cca5189a7284c5fa879fc42e65e1fb
hotelufo <baarbarabottazzi@gmail.com>
2020-09-23 19:59:27 -0300

    carousel+texto

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-bootstrap/Carousel'
  
 class DemoCarousel extends Component {
     render() {
         return (
             <Carousel>
-                <div>
-                    <img src="https://plantasyflores.online/wp-content/uploads/2017/08/sunflower-1627193_1920-copia.jpg" />
-                    <p className="legend">Summer</p>
-                </div>
-                <div>
-                    <img src="https://www.jardineriaon.com/wp-content/uploads/2019/03/Flores-de-narcisos-blancos-abiertas-totalmente.png" />
-                    <p className="legend">Spring </p>
-                </div>
-                <div>
-                    <img src="https://www.ecured.cu/images/1/1f/Aster_escocia_2.jpg" />
-                    <p className="legend">Autumn</p>
-                </div>
-                <div>
-                    <img src="https://paramijardin.s3.fr-par.scw.cloud/2017/10/IMG_20171026_170713544-e1509224989426-800x449.jpg" />
-                    <p className="legend">Winter</p>
-                </div>
-            </Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://miro.medium.com/max/3840/1*oxzfllcH_OqAppRptmWa8A.jpeg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Spring</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://s1.1zoom.me/big0/312/Summer_Coast_Sea_Sky_508163.jpg"
+                alt="Third slide"
+              />
+          
+              <Carousel.Caption>
+                <h3>Summer</h3>
+             
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://cdn.britannica.com/88/137188-050-8C779D64/Boston-Public-Garden.jpg"
+                alt="Third slide"
+              />
+          
+              <Carousel.Caption>
+                <h3>Autumn</h3>
+              
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQURKjKxQmHE6kBzm2Xs2P1HBONKdLn2txosw&usqp=CAU"
+                alt="Third slide"
+              />
+          
+              <Carousel.Caption>
+                <h3>Winter</h3>
+              
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         );
     }
 };
--- src/components/home.js
+++ src/components/home.js
@@ -7,10 +7,25 @@ class Home extends Component {
     render() { 
         return ( 
             <div className="home">
-                <br/>
-                <h4>Seasons</h4>        
-                <img src="https://c.tadst.com/gfx/750x500/four-seasons.jpg?1"/>
+                <br/>      
+ 
+              <img src="https://c.tadst.com/gfx/750x500/four-seasons.jpg?1"/>
+<h4>Why seasons occur?</h4>
+<p>
+Seasons occur because of the tilt of the Earth’s axis. The axis of rotation of the Earth is not straight, but it is slightly tilted, which give rise to seasons.
+earth-tilt
+<h4>How do seasons change?</h4>
+<p>
+Our Earth completes one revolution in the orbit around the Sun in one year or 365 days. The sunlight that each part of the Earth receive, changes as the Earth moves in the orbit. This is because of tilt in the Earth axis of rotation. Due to this tilt, the Sun points northern hemisphere for half year and southern hemisphere for the second half year. As a result, the season of the Earth changes. The season of the northern hemisphere is opposite to that of the southern hemisphere.</p>
+<h4>Interesting Facts</h4>
+<p>
+<li>Animals find the winter hard because it is difficult for them to find food.
+</li>
+<li>Plants lose their leaves in autumn in response to decreasing length of the day.
+</li>
+</p>
 
+</p>
     
         <DemoCarousel/>
         
